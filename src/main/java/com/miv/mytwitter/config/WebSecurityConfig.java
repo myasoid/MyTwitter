eed60e050b1c7bee704d32d5b0b82b0f1b288327
myasoid <ivan.myasoid@gmail.com>
2015-11-04 12:17:14 +0200

    2015-11-04

--- src/main/java/com/miv/mytwitter/config/WebSecurityConfig.java
+++ src/main/java/com/miv/mytwitter/config/WebSecurityConfig.java
@@ -4,46 +4,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@EnableWebSecurity
 @Configuration
-class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/signin", "/signup", "/user/create", "/webjars/**").permitAll()
-                // .antMatchers("/users/**").hasAuthority("ADMIN")
-                .anyRequest().fullyAuthenticated()
+        http
+                .authorizeRequests()
+                .antMatchers("/signup", "/login", "/webjars/**").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/signin")
-                .failureUrl("/signin?error")
-                .usernameParameter("login")
+                .loginPage("/login")
+                        //  .defaultSuccessUrl("/")
                 .permitAll()
                 .and()
                 .logout()
-                .logoutUrl("/sigout")
-                .deleteCookies("remember-me")
-                .logoutSuccessUrl("/signin")
-                .permitAll()
-                .and()
-                .rememberMe();
+                .permitAll();
     }
 
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .userDetailsService(userDetailsService)
                 .passwordEncoder(new BCryptPasswordEncoder());
     }
 
 }
-
--- src/main/java/com/miv/mytwitter/controller/IndexController.java
+++ src/main/java/com/miv/mytwitter/controller/IndexController.java
@@ -1,13 +1,28 @@
 package com.miv.mytwitter.controller;
 
+import com.miv.mytwitter.domain.User;
+import com.miv.mytwitter.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
-    String index() {
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String showIndex(final HttpServletRequest request, Principal principal, Model model) {
+        final String currentUserLogin = principal.getName();
+        User user = userService.findByLogin(currentUserLogin);
+
+
         return "index";
     }
 }
