60807d252481f8501a3618b6d4b771e564ec0c6e
myasoid <ivan.myasoid@gmail.com>
2015-11-09 12:42:58 +0200

    2015-11-09

--- src/main/java/com/miv/mytwitter/config/WebMvcConfig.java
+++ src/main/java/com/miv/mytwitter/config/WebMvcConfig.java
@@ -14,6 +14,7 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/signup").setViewName("signup");
+        registry.addViewController("/{username}").setViewName("profile");
     }
 
 }
--- src/main/java/com/miv/mytwitter/controller/ProfileController.java
+++ src/main/java/com/miv/mytwitter/controller/ProfileController.java
@@ -4,14 +4,12 @@ package com.miv.mytwitter.controller;
 import com.miv.mytwitter.domain.User;
 import com.miv.mytwitter.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 import java.security.Principal;
 
@@ -21,15 +19,15 @@ public class ProfileController {
     @Autowired
     UserService userService;
 
-//    @RequestMapping(value = "/{login}", method = RequestMethod.GET)
-//    @ResponseBody
-//    @Transactional
-//    public String getProfile(final HttpServletRequest request, Principal principal, Model model){
-//        final String currentUserLogin = principal.getName();
-//        User user = userService.findByLogin(currentUserLogin);
-//        return "profile";
-//    }
-//
+    @PreAuthorize("@userService.canAccessUser(principal, #username)")
+    @RequestMapping(value = "/{username}\b(?<!/login|/signup)", method = RequestMethod.GET)
+    @Transactional
+    public String getProfile(final HttpServletRequest request, Principal principal, Model model) {
+        final String currentUserLogin = principal.getName();
+        User user = userService.findByLogin(currentUserLogin);
+        return "profile";
+    }
+
 //    @RequestMapping(value = "/{login}/following", method = RequestMethod.GET)
 //    @ResponseBody
 //    @Transactional
